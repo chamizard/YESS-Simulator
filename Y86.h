9fc75558fbe691a96ecf11c735cd2ff65e234ccc
barbame <barbame@appstate.edu>
2021-02-22 16:01:17 -0500

    Updated files

--- Y86.h
+++ Y86.h
@@ -1,6 +1,6 @@
 /*
     File:   Y86.h
-    Desc:   Y86 class declaration 
+    Desc:   Y86 constants 
     
     Author: efb
 */
@@ -8,28 +8,40 @@
 #ifndef Y86_H
 #define Y86_H
 
+#include <iostream>
+#include <cstdint>
 #include <string>
-
 #include "Memory.h"
 #include "ProgRegisters.h"
 
 
+//class PipeStage;  // Forward declare because of PipeStage reference array
 
 class Y86 {
 
     Memory              memory;          
     ProgRegisters       regs;
-    
+	
     /* Private member functions */
-    
-	/* Public interface */
+	bool 		readFile		(std::ifstream& infile);
+	int 		writeMemory		(std::string inst, uint64_t address);
+    void 		getLine			(uint64_t *, uint64_t);
+	
+	std::string getFlagsString	(void); // in Y86trace.cpp
+	
+    /* Public member functions */
     public:
         Y86();
         void reset();
-	void clockP0();
-	void clockP1();
-        Memory& getMemory(){return memory;}  			// used to test memory
-        ProgRegisters& getProgRegisters(){return regs;} // used to test registers
+		void clockP0();
+		void clockP1();
+		bool load(char *fname);  // takes name of a file as a C string
+		void dumpMemory();			
+		void dumpProgramRegisters();
+		void dumpProcessorRegisters();
+		
+        Memory& getMemory(){return memory;}  
+        ProgRegisters& getProgRegisters(){return regs;}
 };
 
 #endif
