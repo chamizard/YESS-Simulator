2c1507b2685214800037acd81b0ea8b2c40eec51
manncp <manncp@appstate.edu>
2021-02-22 16:45:23 -0500

    Added method stubs to Y86Loader.cpp

--- Y86Loader.cpp
+++ Y86Loader.cpp
@@ -6,8 +6,116 @@
 */
 #include <cstdint>
 
-namespace Y86Loader{
+namespace Y86Loader {
+
+/*
+
+*/
+bool isValidFileName(string input) {
 
 
-    
 }
+
+/*
+
+*/
+void readfile(std::ifstream infile) {
+
+
+}
+
+/*
+
+*/
+bool checkLine() {
+
+
+}
+
+/*
+
+*/
+bool hasValidAddress() {
+
+
+}
+
+/*
+
+*/
+bool isCommentLine() {
+
+
+}
+
+/*
+
+*/
+bool isBlankLine() {
+
+
+}
+
+/*
+
+*/
+bool hasData() {
+
+
+}
+
+/*
+
+*/
+uint64_t hasValidData() {
+
+
+}
+
+}
+
+/*
+
+*/
+uint64_t getAddress(string input) {
+
+
+}
+
+}
+
+/*
+
+*/
+uint64_t hasSpaces(string input, int start, int end) {
+
+
+}
+
+}
+
+/*
+
+*/
+bool checkHex(string input, int start, int end) {
+
+
+}
+
+/*
+
+*/
+bool storeData(string input, int numBytes) {
+
+
+}
+
+/*
+
+*/
+bool storeByte(string input, uint64_t byteAddress) {
+
+
+}
+
+} // end namespace Y86Loader
